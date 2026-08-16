4bb4abdefa7461f3b36d7bb44088dff248f08f92
buildwithchristian <245952046+christiandejesus320-droid@users.noreply.github.com>
2026-08-16 18:19:30 -0400

    feat(video): registrar composición Security Arcade

--- meridian-video/src/Root.tsx
+++ meridian-video/src/Root.tsx
@@ -1,9 +1,11 @@
 import "./index.css";
 import { MyComposition } from "./Composition";
+import { MeridianSecurityGameComposition } from "./SecurityGame";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      <MeridianSecurityGameComposition />
       <MyComposition />
     </>
   );
